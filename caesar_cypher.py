390fe3c2e3b1c79e5dad4a2af72f8c0f614cbc47
Flávio Tassan <tassanf@gmail.com>
2019-01-07 23:00:12 -0200

    more tests

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -19,13 +19,21 @@ def alphabet_uppercase():
     return letters
 
 
-def letter_position(letter):
+def letter_position(letter, alphabet):
+    return alphabet.index(letter)
+
+
+def move_letter(letter, direction, n, alphabet):
     pass
 
-
-def cyphered_letter_position(letter):
+def cyphered_letter_position(letter, rotated_alphabet):
     pass
 
+def rotate_letter(letter, direction, n, alphabet):
+    return ['a']
 
 def rotate_alphabet(alphabet, direction, n):
+    pass
+
+def replace_special_character(letter):
     pass
--- tests/test_cypher.py
+++ tests/test_cypher.py
@@ -11,10 +11,9 @@ from .context import caesar_cypher as cypher
 
 import unittest
 
-alphabet = []
+
 rotated_alphabet = []
-direction = 'left'
-n = 3
+
 
 class BasicTestSuite(unittest.TestCase):
     """Basic Test cases."""
@@ -22,17 +21,44 @@ class BasicTestSuite(unittest.TestCase):
     def test_alphabet_lowercase(self):
         assert cypher.alphabet_lowercase() == ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+
     def test_alphabet_uppercase(self):
         assert cypher.alphabet_uppercase() == ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-    def test_letter_position(self):
-        assert cypher.letter_position('A') == 0
 
-    def test_cyphered_letter_position(self):
-        assert cypher.cyphered_letter_position('A') == 2
+    def test_letter_position(self):
+        alphabet = cypher.alphabet_uppercase()
+        assert cypher.letter_position('A', alphabet) == 0
+
+    
+    def test_move_letter(self):
+        """ 
+            Mover uma letra N posiçoes na lista e retornar a nova posicao
+        """
+        letter = 'A'
+        direction = 'left'
+        n = 3
+        alphabet = []
+        assert cypher.move_letter(letter, direction, n, alphabet) == 3
+
+    def test_rotate_letter(self):
+        letter = 'A'
+        direction = 'left'
+        n = 3
+        alphabet = cypher.alphabet_uppercase()
+        letter_rotate = cypher.rotate_letter(letter, direction, n, alphabet)
+        assert cypher.letter_position(letter, letter_rotate) == n
 
     def test_rotate_alphabet(self):
+        alphabet = []
+        direction = 'left'
+        n = 3
         assert cypher.rotate_alphabet(alphabet, direction, n) == rotated_alphabet
 
+    def test_cyphered_letter_position(self):
+        assert cypher.cyphered_letter_position('A', rotated_alphabet) == 2
 
 
+    def test_replace_special_character(self):
+        letter= 'Á'
+        assert cypher.replace_special_character(letter) == 'A'
